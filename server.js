daf0b2773aaa395e4ee3915f2c90d4248248e6ba
Yasmin Almeida <yasminatr2004@gmail.com>
2024-10-31 12:34:07 -0300

    att user, cor, acervo

--- server.js
+++ server.js
@@ -21,24 +21,69 @@ app.post('/buscar-produto', (req, res) => {
     const produto = data.find(item => item.codigo.toString() === codigo);
 
     if (produto) {
-        const nomeImagem = produto.imagem; // Assumindo que você tem uma coluna 'imagem'
         const resposta = {
-            estacao: produto.estacao,
+            rua: produto.rua,
             rack: produto.rack,
             coluna: produto.coluna,
             posicao: produto.posicao,
             descricao: produto.descricao,
-            imagem: nomeImagem
+            imagem: produto.imagem // Supondo que você tenha uma coluna 'imagem'
         };
-        console.log('Resposta:', resposta); // Adicione esta linha para ver a resposta
+        console.log('Resposta:', resposta); // Para verificar a resposta
         res.json(resposta);
     } else {
         res.status(404).json({ mensagem: 'Produto não encontrado.' });
     }
 });
 
+// Endpoint para atualizar produto
+app.post('/atualizar-produto', (req, res) => {
+    const { codigo, rua, rack, coluna, posicao, descricao } = req.body;
+
+    // Ler o arquivo Excel
+    const workbook = xlsx.readFile('posicaoEstoque.xls');
+    const sheetName = 'Planilha1';
+    const worksheet = workbook.Sheets[sheetName];
+    const data = xlsx.utils.sheet_to_json(worksheet);
+
+    // Encontrar o índice do produto
+    const index = data.findIndex(item => item.codigo.toString() === codigo);
+
+    if (index === -1) {
+        return res.status(404).json({ mensagem: 'Produto não encontrado.' });
+    }
+
+    // Atualizar o produto
+    data[index] = {
+        ...data[index],
+        rua,
+        rack,
+        coluna,
+        posicao,
+        descricao,
+    };
+
+    // Salvar as alterações de volta no arquivo Excel
+    const newWorksheet = xlsx.utils.json_to_sheet(data);
+    workbook.Sheets[sheetName] = newWorksheet;
+    xlsx.writeFile(workbook, 'posicaoEstoque.xls');
+
+    res.json({ mensagem: 'Produto atualizado com sucesso.' });
+});
+
+// Rota principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota de administração
+app.get('/adm', (req, res) => {
+    res.sendFile(path.join(__dirname, 'adm.html'));
+});
+
+// Rota de CRUD
+app.get('/crud', (req, res) => {
+    res.sendFile(path.join(__dirname, 'crud.html'));
 });
 
 app.use('/acervo', express.static(path.join(__dirname, 'acervo')));
